9a8cb744d38015eceb1c0f0071b92a7893671878
Mandy Alford <mandyalford@gmail.com>
2020-01-10 20:53:37 -0700

    Build ToDoList class

---
+++ todo-list.js
@@ -0,0 +1,20 @@
+class TodoList {
+  constructor(){
+    this.id = ;
+    this.title = "";
+    this.tasks = {};
+    this.urgent = false;
+  }
+  saveToStorage(){
+
+  }
+  deleteFromStorage(){
+
+  }
+  updateToDo(){
+
+  }
+  updateTask(){
+
+  }
+}
